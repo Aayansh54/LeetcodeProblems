43e8f8b8899be5435dd9cfe9bb117b11c640dde4
Aayansh <karnaayansh@gmail.com>
2026-08-29 21:21:47 +0530

    Time: 3 ms (3.68%), Space: 41.9 MB (88.97%) - LeetHub

---
+++ 0878-nth-magical-number/0878-nth-magical-number.java
@@ -0,0 +1,38 @@
+class Solution { 
+public long lcm(int a , int b){ 
+    int lcm = 1;
+    int factor = 2;
+    while(factor <= Math.min(a,b)){
+    
+    if(a % factor == 0 && b % factor == 0){
+        a/=factor;
+        b/=factor;
+        lcm *= factor;
+    }else 
+    factor++;
+  }
+  return (long)lcm * a * b;
+} 
+
+    public int nthMagicalNumber(int n, int a, int b) {
+        long left = 1;
+        long right = (long)n * a * b;
+        long ans = 0;
+        int mod = 1_000_000_007;
+        long LCM = lcm(a,b);
+        while(left <= right){
+            long mid = left + (right - left) /2;
+            long aDivNums = mid / a;
+            long bDivNums = mid / b;
+            long abDivNums = mid /LCM ;
+            long totalNums = aDivNums + bDivNums - abDivNums;
+            if(totalNums >= n){
+                ans = mid;
+                right = mid - 1;
+            }
+            else 
+                left = mid + 1;
+        }
+        return (int)(ans % mod) ;
+    }
+}
