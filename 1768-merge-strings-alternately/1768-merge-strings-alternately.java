fcda0bb81a9f31c592925dd827c858b2322e1227
Aayansh <karnaayansh@gmail.com>
2026-09-05 19:21:47 +0530

    Time: 4 ms (19.76%), Space: 44 MB (13.91%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -6,18 +6,15 @@ class Solution {
         int i = 0 ; 
         int j = 0;
         while(i < n1 && j < n2){
-            ans += word1.charAt(i);
-            ans += word2.charAt(j);
-            i++;
-            j++;
+            ans += word1.charAt(i++);
+            ans += word2.charAt(j++);
         }
         while(i<n1){
-            ans +=  word1.charAt(i);
-            i++;
+            ans +=  word1.charAt(i++);
+           
         }
         while(j<n2){
-            ans +=  word2.charAt(j);
-            j++;
+            ans +=  word2.charAt(j++);
         }
         return ans;
     }
