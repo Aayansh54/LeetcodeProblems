fda193e49c1e46e3a585c409feb7d8f9ba3e4ce6
Aayansh <karnaayansh@gmail.com>
2026-09-06 15:31:46 +0530

    Time: 0 ms (100%), Space: 80.3 MB (66.18%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -4,11 +4,11 @@ class Solution {
         int j = 0;
         int n = nums1.length;
         int m = nums2.length;
+        if(nums1[n-1] < nums2[0] || nums2[m-1] < nums1[0]) return -1;
         while(i < n && j < m){
             if(nums1[i] == nums2[j]) return nums1[i];
             else if(nums1[i] > nums2[j]){
                 j++;
-                continue;
             }
             else 
             i++;
