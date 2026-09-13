09af087b79ff3c97bbda99f22763b5da14d42d67
Aayansh <karnaayansh@gmail.com>
2026-09-13 17:46:42 +0530

    Time: 28 ms (48.46%), Space: 45.9 MB (7.67%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -3,30 +3,28 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int triplets = 0;
-        for(int i = 0 ; i < n-2 ; i++ ){
-            if(nums[i] == 0) continue;
-            for(int j = i+1; j < n-1 ; j++){
-                if(nums[j] == 0) continue;
-                int sum = nums[i] + nums[j];
-                int k = BinSearch(j,n,sum,nums);
-                triplets += k - j;
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+            if (nums[k] == 0)
+                continue;
+            while (i < j) {
+                if (nums[i] == 0) {
+                    i++;
+                    continue;
+                }
+                if (nums[j] == 0) {
+                    j--;
+                    continue;
+                }
+                if (nums[i] + nums[j] > nums[k]) {
+                    triplets += j - i;
+                    j--;
+                } else
+                    i++;
             }
+
         }
         return triplets;
-    }
-    int BinSearch(int j , int n , int sum,int[] nums){
-        int ans = j;
-        int left = j + 1;
-        int right = n - 1;
-        while(left <= right){
-            int mid =( left + right )/2;
-            if(nums[mid] < sum){
-                left = mid + 1;
-                ans = mid;
-            }
-            else
-               right = mid - 1;
-        }
-        return ans;
     }
 }
